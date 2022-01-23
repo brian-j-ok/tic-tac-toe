332b3d34c09c9827328bc3607e60288348077beb
Brian O'Kelly <brianjrok@gmail.com>
2022-01-22 20:40:58 -0500

    Fixed errors with player inputs (ability to play in previously played spot and invalid inputs); add win screen; fixed logic for determining if a player had won the game
    Added TODO comments for future changes to be made

--- app.rb
+++ app.rb
@@ -1,60 +1,78 @@
 class Game
-    WIN_COMBINATIONS = [ 
-        [0,1,2], # top_row 
-        [3,4,5], # middle_row 
-        [6,7,8], # bottom_row 
-        [0,3,6], # left_column 
-        [1,4,7], # center_column 
-        [2,5,8], # right_column 
-        [0,4,8], # left_diagonal 
-        [6,4,2] # right_diagonal 
-        ]
+    WIN_COMBINATIONS = [
+        [0, 1, 2], # top_row
+        [3, 4, 5], # middle_row
+        [6, 7, 8], # bottom_row
+        [0, 3, 6], # left_column
+        [1, 4, 7], # center_column
+        [2, 5, 8], # right_column
+        [0, 4, 8], # left_diagonal
+        [6, 4, 2] # right_diagonal
+    ]
 
     @@board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    @@winner = ""
 
     def initialize()
         puts "What is the first player's name?"
         player1_name = gets.chomp
-        @player1 = Player.new(player1_name)
+        @player1 = Player.new(player1_name, "X")
         puts "What is the second player's name?"
         player2_name = gets.chomp
-        @player2 = Player.new(player2_name)
+        @player2 = Player.new(player2_name, "O")
         draw_board
         play
     end
 
     def draw_board
+        puts
         puts "#{@@board[0]} | #{@@board[1]} | #{@@board[2]}"
-        puts '------------'
+        puts '----------'
         puts "#{@@board[3]} | #{@@board[4]} | #{@@board[5]}"
-        puts '------------'
+        puts '----------'
         puts "#{@@board[6]} | #{@@board[7]} | #{@@board[8]}"
-        puts '------------'
+        puts '----------'
+        puts
     end
 
+    # TODO: See if you can rework the turn boolean into something cleaner
     def play
         turn = true
 
         while !victory?
             if turn
-                puts "Player 1 pick the position you would like to play at? (1-9)"
-                position = gets.chomp
-                @@board[position.to_i - 1] = "X"
+                player_move(@player1)
                 turn = !turn
             elsif !turn
-                puts "Player 2 pick the position you would like to play at? (1-9)"
-                position = gets.chomp
-                @@board[position.to_i - 1] = "O"
+                player_move(@player2)
                 turn = !turn
             end
             draw_board
         end
+
+        puts "Congrats " + @@winner + "! You win the game!!!"
     end
 
+    # TODO: Clean up if statement and error handling (specific feedback to user regarding why the input is invalid)
+    def player_move (player)
+        valid_move = false
+        while !valid_move do
+            puts player.player_name + "'s turn! At what position would you like to play?"
+            position = gets.chomp
+            if @@board[position.to_i - 1] != 'X' && @@board[position.to_i - 1] != 'O' && position.to_i < 10 && position.to_i > 0
+                @@board[position.to_i - 1] = player.player_symbol
+                valid_move = true
+            else
+                puts "Invalid input."
+            end
+        end
+    end
+
+    # TODO: Add a win count for repeat games
     def victory?
         player1_positions = Array.new
         player2_positions = Array.new
-        
+
         @@board.each_with_index do |piece_type, index|
             if piece_type == "X"
                 player1_positions.push(index)
@@ -65,19 +83,25 @@ class Game
 
         WIN_COMBINATIONS.each do |win_array|
             if (win_array - player1_positions).empty?
+                @@winner = @player1.player_name
                 return true
             elsif (win_array - player2_positions).empty?
+                @@winner = @player2.player_name
                 return true
-            else
-                return false
             end
         end
+
+        false
     end
 end
 
+# TODO: Store wincount in the player class
 class Player
-    def initialize(name)
+    attr_reader :player_name, :player_symbol
+
+    def initialize(name, player_symbol)
         @player_name = name
+        @player_symbol = player_symbol
     end
 end
 
